1310a595fdf3b57f54135a2899b215d446691f3f
FSS-RECONSTRUCCIONES <faustino.sansegundo@gmail.com>
2025-08-03 10:56:13 +0200

    gracias.js

--- gracias.js
+++ gracias.js
@@ -40,57 +40,5 @@ window.onclick = function(event) {
     closeAllSubmenus();
     }
 }
-document.addEventListener("DOMContentLoaded", function () {
-  const botonFormulario = document.getElementById("mostrar-formulario");
-  const formulario = document.getElementById("formulario-contacto");
-
-  // Asegura que está oculto al cargar
-  formulario.style.display = "none";
-
-  botonFormulario.addEventListener("click", function () {
-    if (formulario.style.display === "none" || formulario.style.display === "") {
-      formulario.style.display = "block";
-    } else {
-      formulario.style.display = "none";
-    }
-  });
-});
 
 
-window.addEventListener('scroll', () => {
-  const img = document.getElementById('imagen-lateral');
-  const formulario = document.getElementById('formulario-contacto');
-
-  // Comprobar si el formulario está visible
-  const formularioVisible = formulario.style.display === 'block';
-
-  if (formularioVisible) {
-    const scrollY = window.scrollY;
-    const desplazamiento = scrollY * 1.09; // ajusta la velocidad aquí
-    img.style.transform = `translateY(${desplazamiento}px)`;
-  } else {
-    // Si el formulario NO está visible, no mover la imagen (la posicionamos en 0)
-    img.style.transform = 'translateY(0)';
-  }
-});
-
-/* Envío de formulario al correo */
-const $form = document.querySelector("#form")
-
-$form.addEventListener("submit", handleSubmit)
-
-async function handleSubmit(event) {
-  event.preventDefault()
-  const form = new FormData(this)
-  const reponse = await fetch(this.action, {
-    method: this.method,
-    body: form,
-    headers: {
-      'Accept': 'application/json'
-    }
-  })
-  if (reponse.ok) {
-    this.reset()
-    alert("Gracias por contactar con nosotros")
-  }
-}
